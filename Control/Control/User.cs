e9ffecfe2c5853e1a2165d8d3d97e088b65d4d30
Doit <duhee7524@naver.com>
2023-10-18 19:21:18 +0900

    ItemsControl

---
+++ Control/Control/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Control
+{
+    internal class User
+    {
+        public object Name { get; private set; }
+        public string Location { get; private set; }
+
+        internal User GenData(string name, string location)
+        {
+            var user = new User();
+            user.Name = name;
+            user.Location = location;
+            return user;
+        }
+    }
+}
